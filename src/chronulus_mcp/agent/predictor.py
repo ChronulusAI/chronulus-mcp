d1db6c8eab104db2487cf94c4ee7b6fc61471917
Jeremy Oldfather <jeremy@chronulus.com>
2025-03-16 12:21:22 -0400

    Fixed num_experts type hint

--- src/chronulus_mcp/agent/predictor.py
+++ src/chronulus_mcp/agent/predictor.py
@@ -17,7 +17,7 @@ async def create_prediction_agent_and_get_predictions(
         )],
         input_data: Annotated[Dict[str, Union[str, dict, List[dict]]], Field(description="The forecast inputs that you will pass to the chronulus agent to make the prediction. The keys of the dict should correspond to the InputField name you provided in input_fields.")],
         ctx: Context,
-        num_experts: Annotated[str, Field(description="The number of experts to consult when forming consensus")],
+        num_experts: Annotated[int, Field(description="The number of experts to consult when forming consensus")],
 ) -> Union[str, Dict[str, Union[dict, str]]]:
     """Queues and retrieves a binary event prediction from Chronulus with a predefined session_id
 
@@ -95,7 +95,7 @@ async def reuse_prediction_agent_and_get_prediction(
         agent_id: Annotated[str, Field(description="The agent_id for the forecasting or prediction use case and previously defined input_data_model")],
         input_data: Annotated[Dict[str, Union[str, dict, List[dict]]], Field(
             description="The forecast inputs that you will pass to the chronulus agent to make the prediction. The keys of the dict should correspond to the InputField name you provided in input_fields.")],
-        num_experts: Annotated[str, Field(description="The number of experts to consult when forming consensus")],
+        num_experts: Annotated[int, Field(description="The number of experts to consult when forming consensus")],
 
 ) -> Union[str, Dict[str, Union[dict, str]]]:
     """Queues and retrieves a binary event prediction from Chronulus with a previously created agent_id
